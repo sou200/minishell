f36be0f511ce075bd6b2540d01f86c0331d39f1b
Faouzi El Hazzat <fel-hazz@e3r7p12.1337.ma>
2023-07-17 11:49:01 +0100

    ewwee

--- test.c
+++ test.c
@@ -3,18 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: serhouni <serhouni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fel-hazz <fel-hazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 01:28:49 by serhouni          #+#    #+#             */
-/*   Updated: 2023/07/15 11:50:47 by serhouni         ###   ########.fr       */
+/*   Updated: 2023/07/17 11:46:23 by fel-hazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include "dirent.h"
+#include <stdio.h>
 
-
-
-int main() {
-    
-    return 0;
+int main(int argc, char *argv[]) {
+DIR *dp = opendir("");
+struct dirent *d;
+while ((d = readdir(dp)) != NULL) {
+	// if (DT_LNK == d->d_type)
+		printf("%d %s %hhu\n", (int) d->d_ino, d->d_name, d->d_type);
+}
+closedir(dp);
+return 0;
 }
